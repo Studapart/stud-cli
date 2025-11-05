a6f275e5d2285397cd3456c621c1b7b3cabfe0d7
PEM-Stud <pierre-emmanuel.manteau@studapart.com>
2025-11-05 14:31:56 +0100

    fixup! chore: Create PHPUnit Test Suite for stud-cli Command Logic [TPW-35]

--- tests/TestKernel.php
+++ tests/TestKernel.php
@@ -4,9 +4,11 @@ namespace App\Tests;
 
 use App\Git\GitRepository;
 use App\Jira\JiraService;
+use App\Process\ProcessFactory;
 
 class TestKernel
 {
     public static ?GitRepository $gitRepository = null;
     public static ?JiraService $jiraService = null;
+    public static ?ProcessFactory $processFactory = null;
 }
